d7e4cd672857f6c978e17e0b54c59341ad9e1703
vytautassvirskas <vytautassvirskas@gmail.com>
2022-11-04 14:20:33 +0200

    updated

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const adviceHeading=document.querySelector(".advice__heading")
 const adviceText=document.querySelector(".advice__text")
 const adviceBtn=document.querySelector(".advice__btn")
+const dice=document.querySelector(".advice__btn-img")
 
-async function getAdvice(){
+const getAdvice = async ()=>{
     try {
-        const resp=await fetch("https://api.adviceslip.com/advice");
+        const resp=await fetch("https://api.adviceslip.com/advice", { cache: "no-cache" });
         const data =await resp.json()
         adviceHeading.innerText="advice "+"#"+data.slip.id
         adviceText.innerText=`"${data.slip.advice}"`
