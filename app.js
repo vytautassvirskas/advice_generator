eda2e44ddbbbf360465117784bf750a32d0b5246
vytautassvirskas <vytautassvirskas@gmail.com>
2022-11-04 11:51:22 +0200

    javascript code added, responsive css

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log("veikia");
+const adviceHeading=document.querySelector(".advice__heading")
+const adviceText=document.querySelector(".advice__text")
+const adviceBtn=document.querySelector(".advice__btn")
+const adviceContainer=document.querySelector(".advice")
+
+async function getAdvice(){
+    try {
+        const resp=await fetch("https://api.adviceslip.com/advice");
+        const data =await resp.json()
+        adviceHeading.innerText="advice "+"#"+data.slip.id
+        adviceText.innerText=`"${data.slip.advice}"`
+        // console.log(data.slip)
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+getAdvice();
+
+adviceBtn.addEventListener("click",getAdvice)
